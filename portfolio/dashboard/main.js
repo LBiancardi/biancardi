2f00059e35f216d030fc239b8ba7d4ac05521224
Luis <luisfbiancardi@gmail.com>
2021-11-07 12:50:38 -0800

    Add dark theme nav menu

--- portfolio/dashboard/main.js
+++ portfolio/dashboard/main.js
@@ -7,3 +7,29 @@ function toggleMenu() {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 }
+
+function darkMode() {
+  const navigation = document.querySelector(".navigation");
+
+  navigation.classList.toggle("dark");
+  changeNavItemColor();
+  changeText();
+}
+
+function changeText() {
+  const theme = document.querySelector("#theme");
+
+  if (theme.innerText == "Dark Theme") {
+    theme.innerText = "Light Theme";
+  } else {
+    theme.innerText = "Dark Theme";
+  }
+}
+
+function changeNavItemColor() {
+  const navigation__item = document.querySelectorAll(".navigation__item");
+
+  for (i = 0; i < navigation__item.length; i++) {
+    navigation__item[i].classList.toggle("dark");
+  }
+}
