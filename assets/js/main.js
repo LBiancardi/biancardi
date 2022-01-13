aef570c9eff708b0c5d4646e52064d1477ce3e66
Luis <luisfbiancardi@gmail.com>
2022-01-13 01:53:21 -0800

    Adjust footer position

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,10 @@ const socialMedia = document.querySelectorAll(".social__media__name");
 socialMedia.forEach((link) => {
   link.addEventListener("mouseover", (target) => {
     link.childNodes[1].style.visibility = "visible";
+    // link.childNodes[1].classList.remove("hide");
   });
   link.addEventListener("mouseout", (target) => {
     link.childNodes[1].style.visibility = "hidden";
+    // link.childNodes[1].classList.add("hide");
   });
 });
