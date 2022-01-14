32250d07b36ce3a4a78704311c52d1816eeddc2c
Luis <luisfbiancardi@gmail.com>
2022-01-13 18:00:46 -0800

    Js function to display different msgs every time it reloads the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
+"use strict";
+
 const socialMedia = document.querySelectorAll(".social__media__name");
+const footerTxt = document.getElementsByClassName("footer__txt");
+const footerMsgs = [
+  "Living, learning and growing one day at a time.",
+  "A computer once beat me at chess, but it was no match for me at kick boxing.",
+  "Looking beyond the present moment (Proverbs 23:18) - Keep Faith",
+];
+
+(() => {
+  const randomNumber = Math.floor(Math.random() * footerMsgs.length);
+  footerTxt[0].innerText = footerMsgs[randomNumber];
+})();
 
 socialMedia.forEach((link) => {
-  link.addEventListener("mouseover", (target) => {
+  link.addEventListener("mouseover", () => {
     link.childNodes[1].style.visibility = "visible";
     // link.childNodes[1].classList.remove("hide");
   });
